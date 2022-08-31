8adaf0508460090ac13bd9bf85b1a2e3ed923a7a
SAURABH YADAV <rsyadav.abss1@gmail.com>
2022-08-31 18:15:45 +0530

    update-profile

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -1,115 +1,115 @@
-// import React from "react";
-// import "../../Profile.css";
-// import Tab from "react-bootstrap/Tab";
-// import Tabs from "react-bootstrap/Tabs";
-// import Image70 from "../../../src/img/image-100.jpg";
-// import Image80 from "../../../src/img/images-200.jpg";
-// function Profile() {
-//   return (
-//     <>
-//       <div className="outer-box" id="sy">
-//         <div
-//           className="inner-box"
-//           style={{
-//             width: "50%",
-//             margin: "3rem auto",
-//           }}
-//         >
-//           <div className="name">
-//             <div className="row" style={{}}>
-//               <div className="col-lg-2 col-md-2 col-sm-2 col-2">
-//                 <span>
-//                   <img
-//                     src={Image80}
-//                     alt=""
-//                     style={{
-//                       borderRadius: "50%",
-//                       width: "100%",
-//                       textAlign: "center",
-//                     }}
-//                   />
-//                 </span>
-//               </div>
-//               <div className="col-lg-10 col-md-10 col-sm-10 col-10">
-//                 <div style={{ margin: "1rem" }}>
-//                   <span className="name1">Mayank Bhati</span>
-//                   <br></br>
-//                   <span>@Memefreak194</span>
-//                 </div>
-//               </div>
-//             </div>
+import React from "react";
+import "../../Profile.css";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
+import Image70 from "../../../src/img/image-100.jpg";
+import Image80 from "../../../src/img/images-200.jpg";
+function Profile() {
+  return (
+    <>
+      <div className="outer-box" id="sy">
+        <div
+          className="inner-box"
+          style={{
+            width: "50%",
+            margin: "3rem auto",
+          }}
+        >
+          <div className="name">
+            <div className="row" style={{}}>
+              <div className="col-lg-2 col-md-2 col-sm-2 col-2">
+                <span>
+                  <img
+                    src={Image80}
+                    alt=""
+                    style={{
+                      borderRadius: "50%",
+                      width: "100%",
+                      textAlign: "center",
+                    }}
+                  />
+                </span>
+              </div>
+              <div className="col-lg-10 col-md-10 col-sm-10 col-10">
+                <div style={{ margin: "1rem" }}>
+                  <span className="name1">Mayank Bhati</span>
+                  <br></br>
+                  <span>@Memefreak194</span>
+                </div>
+              </div>
+            </div>
 
-//             <span>My Funny Collection</span>
-//           </div>
-//           <div className="tab">
-//             {" "}
-//             <Tabs
-//               defaultActiveKey="Posts"
-//               id="uncontrolled-tab-example"
-//               className="mb-3"
-//             >
-//               <Tab eventKey="Posts" title="Posts">
-//                 {" "}
-//                 <div
-//                   className="post"
-//                   style={{ textAlign: "center", padding: "3rem" }}
-//                 >
-//                   <span>No Posts</span>
-//                   <br></br>
-//                   <span>Let's make something creative for fun!</span>
-//                   <br></br>
-//                   <button className="post123">
-//                     <svg
-//                       xmlns="http://www.w3.org/2000/svg"
-//                       viewBox="0 0 13 13"
-//                       role="presentation"
-//                       data-v-e9db3ad8=""
-//                     >
-//                       <g
-//                         fill="white"
-//                         data-v-e9db3ad8=""
-//                         style={{ fill: "white" }}
-//                       >
-//                         <path d="M0 10.5V13H2.5L9.87333 5.62665L7.37333 3.12666L0 10.5ZM11.8067 3.69332C12.0667 3.43332 12.0667 3.01332 11.8067 2.75332L10.2467 1.19332C9.98667 0.933322 9.56667 0.933322 9.30667 1.19332L8.08667 2.41332L10.5867 4.91332L11.8067 3.69332Z"></path>
-//                       </g>
-//                     </svg>
-//                     Post
-//                   </button>
-//                 </div>
-//               </Tab>
-//               <Tab eventKey="Comments" title="Comments">
-//                 {" "}
-//                 <div
-//                   className="post"
-//                   style={{ textAlign: "center", padding: "3rem" }}
-//                 >
-//                   <span>No commented posts</span>
-//                 </div>
-//               </Tab>
-//               <Tab eventKey="Upvotes" title="Upvotes">
-//                 {" "}
-//                 <div
-//                   className="post"
-//                   style={{ textAlign: "center", padding: "3rem" }}
-//                 >
-//                   <span>No upvoted posts</span>
-//                 </div>
-//               </Tab>
-//               <Tab eventKey="Saved" title="Saved">
-//                 {" "}
-//                 <div
-//                   className="post"
-//                   style={{ textAlign: "center", padding: "3rem" }}
-//                 >
-//                   <span>No saved posts</span>
-//                 </div>
-//               </Tab>
-//             </Tabs>
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
+            <span>My Funny Collection</span>
+          </div>
+          <div className="tab">
+            {" "}
+            <Tabs
+              defaultActiveKey="Posts"
+              id="uncontrolled-tab-example"
+              className="mb-3"
+            >
+              <Tab eventKey="Posts" title="Posts">
+                {" "}
+                <div
+                  className="post"
+                  style={{ textAlign: "center", padding: "3rem" }}
+                >
+                  <span>No Posts</span>
+                  <br></br>
+                  <span>Let's make something creative for fun!</span>
+                  <br></br>
+                  <button className="post123">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 13 13"
+                      role="presentation"
+                      data-v-e9db3ad8=""
+                    >
+                      <g
+                        fill="white"
+                        data-v-e9db3ad8=""
+                        style={{ fill: "white" }}
+                      >
+                        <path d="M0 10.5V13H2.5L9.87333 5.62665L7.37333 3.12666L0 10.5ZM11.8067 3.69332C12.0667 3.43332 12.0667 3.01332 11.8067 2.75332L10.2467 1.19332C9.98667 0.933322 9.56667 0.933322 9.30667 1.19332L8.08667 2.41332L10.5867 4.91332L11.8067 3.69332Z"></path>
+                      </g>
+                    </svg>
+                    Post
+                  </button>
+                </div>
+              </Tab>
+              <Tab eventKey="Comments" title="Comments">
+                {" "}
+                <div
+                  className="post"
+                  style={{ textAlign: "center", padding: "3rem" }}
+                >
+                  <span>No commented posts</span>
+                </div>
+              </Tab>
+              <Tab eventKey="Upvotes" title="Upvotes">
+                {" "}
+                <div
+                  className="post"
+                  style={{ textAlign: "center", padding: "3rem" }}
+                >
+                  <span>No upvoted posts</span>
+                </div>
+              </Tab>
+              <Tab eventKey="Saved" title="Saved">
+                {" "}
+                <div
+                  className="post"
+                  style={{ textAlign: "center", padding: "3rem" }}
+                >
+                  <span>No saved posts</span>
+                </div>
+              </Tab>
+            </Tabs>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
 
-// export default Profile;
+export default Profile;
